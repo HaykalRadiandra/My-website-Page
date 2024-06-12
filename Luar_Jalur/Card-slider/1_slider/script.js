d7b3f83e85a7418405e3abac19bc484c7fe2bc8d
HaykalRadiandra <rthaykal@gmail.com>
2024-06-12 10:55:12 +0700

    update all

---
+++ Luar_Jalur/Card-slider/1_slider/script.js
@@ -0,0 +1,37 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    // slidesPerGroup: 3,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor: 'true',
+    // loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+
+        520: {
+            slidesPerView: 2,
+        },
+
+        950: {
+            slidesPerView: 3,
+        },
+
+        1200: {
+            slidesPerView: 4,
+        }
+    },
+  });
